19b936af7d28a81876d04ed18c75c90ed52c3d50
Abdul Rahman <abdulrahman161004@gmail.com>
2025-03-04 16:47:45 +0530

    Created Scroll Reveal in JavaScript

--- script.js
+++ script.js
@@ -26,3 +26,45 @@ function toggleAccordion(accordionId) {
     answer.style.maxHeight = isActive ? answer.scrollHeight + "px" : "0";
     arrow.className = isActive ? "fas fa-minus" : "fas fa-plus";
 }
+
+// Scroll Reveal
+const sr = ScrollReveal({
+    origin: "bottom",
+    distance: "30px",
+    duration: 1000,
+    delay: 100,
+    easing: "ease-in-out"
+});
+
+sr.reveal(".slogan-1");
+sr.reveal(".slogan-2", { delay: 500 });
+
+// New Arival Boxes
+sr.reveal(".new-arrivals-box:nth-child(1)");
+sr.reveal(".new-arrivals-box:nth-child(2)", { delay: 300 });
+sr.reveal(".new-arrivals-box:nth-child(3)", { delay: 500 });
+
+sr.reveal(".about-title h1", { origin: "left" });
+sr.reveal(".about-title p ", { origin: "right" });
+sr.reveal(".about-box");
+
+// Projects
+sr.reveal(".project h1");
+sr.reveal(".project-box");
+
+// FAQ & footer Reveal
+sr.reveal(".FAQ");
+sr.reveal(".footer-headline");
+sr.reveal(".footer-newsletter", { origin: "left" });
+sr.reveal(".footer-container", { origin: "right" });
+sr.reveal(".socials");
+
+// Product Page
+sr.reveal(".product-page img", { origin: "top" });
+sr.reveal(".product-title", { delay: 500 });
+sr.reveal(".product-container", { delay: 1000 });
+
+// Contact Page
+sr.reveal(".contact h1");
+sr.reveal(".contact-container", { delay: 500 });
+sr.reveal(".contact img", { delay: 1000 });
