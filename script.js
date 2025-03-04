e0db16029f36268a480840d0d04b844e17df9123
Abdul Rahman <abdulrahman161004@gmail.com>
2025-03-04 12:13:10 +0530

    Write JS code for FAQ section

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ document.querySelector(".close-btn").addEventListener("click", toggleMenu);
 document
     .querySelectorAll("menu a")
     .forEach(link => link.addEventListener("click"), toggleMenu);
+
+// FAQ
+function toggleAccordion(accordionId) {
+    const accordionItem = document.getElementById(accordionId);
+    const answer = accordionItem.querySelector(".answer");
+    const arrow = accordionItem.querySelector(".fas");
+    const isActive = accordionItem.classList.toggle("acitve");
+
+    answer.style.maxHeight = isActive ? answer.scrollHeight + "px" : "0";
+    arrow.className = isActive ? "fas fa-minus" : "fas fa-plus";
+}
